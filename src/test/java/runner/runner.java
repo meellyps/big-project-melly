39a5ea44758c5d34cc08fa6d103cb45091634fde
Melly Puspita Sari <mellypuspita21@gmail.com>
2022-10-10 10:48:14 +0700

    add send email

--- src/test/java/runner/runner.java
+++ src/test/java/runner/runner.java
@@ -2,6 +2,8 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterSuite;
+import utils.SendEmail;
 
 public class runner {
 
@@ -9,7 +11,19 @@ public class runner {
             features = "src/test/resources/feature",
             glue= {"stepDef"},
             plugin = {"pretty", "html:target/cucumber-report.html"},
-            tags = "@board_001"
+            tags = "@board_001 or @board_002"
     )
-    public static class run extends AbstractTestNGCucumberTests {}
+    public static class run extends AbstractTestNGCucumberTests {
+        @AfterSuite
+        public void sendReportEmail() {
+            String[] args = new String[] {
+                    "mellypuspita21@gmail.com",
+                    "de.boyz99@gmail.com",
+                    "Big Project Report - Melly",
+                    "Big Project Report",
+                    "D:\\adiladios\\project\\java\\big-project-melly\\target\\cucumber-report.html"
+            };
+            SendEmail.main(args);
+        }
+    }
 }
